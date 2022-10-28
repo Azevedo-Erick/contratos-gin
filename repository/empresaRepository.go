7f29f1980fab61cb9b94be93294449c590a16770
Azevedo-Erick <azevedoerick762@gmail.com>
2022-10-28 12:27:32 -0300

    FEAT: CRUDS completos

--- repository/empresaRepository.go
+++ repository/empresaRepository.go
@@ -1,1 +1,48 @@
 package repository
+
+import (
+	"test/database"
+	"test/models"
+)
+
+func GetEmpresaById(id int) (models.Empresa, error) {
+	var empresa models.Empresa
+	err := database.Database.Db.Where("id = ?", id).First(&empresa).Error
+	if err != nil {
+		return empresa, err
+	}
+	return empresa, nil
+}
+
+func GetAllEmpresas() ([]models.Empresa, error) {
+	var empresas []models.Empresa
+	err := database.Database.Db.Find(&empresas).Error
+	if err != nil {
+		return empresas, err
+	}
+	return empresas, nil
+}
+
+func CreateEmpresa(empresa models.Empresa) (models.Empresa, error) {
+	err := database.Database.Db.Create(&empresa).Error
+	if err != nil {
+		return empresa, err
+	}
+	return empresa, nil
+}
+
+func UpdateEmpresa(empresa models.Empresa) (models.Empresa, error) {
+	err := database.Database.Db.Save(&empresa).Error
+	if err != nil {
+		return empresa, err
+	}
+	return empresa, nil
+}
+
+func DeleteEmpresa(empresa models.Empresa) error {
+	err := database.Database.Db.Delete(&empresa).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- repository/responsavelEmpresaRepository.go
+++ repository/responsavelEmpresaRepository.go
@@ -1,1 +1,31 @@
 package repository
+
+import (
+	"test/database"
+	"test/models"
+)
+
+func GetResponsavelEmpresaById(id int) (responsavelEmpresa models.ResponsavelEmpresa, err error) {
+	err = database.Database.Db.Find(&responsavelEmpresa, "id=", id).Error
+	return
+}
+
+func CreateResponsavelEmpresa(responsavelEmpresa models.ResponsavelEmpresa) (err error) {
+	err = database.Database.Db.Create(&responsavelEmpresa).Error
+	return
+}
+
+func UpdateResponsavelEmpresa(responsavelEmpresa models.ResponsavelEmpresa) (err error) {
+	err = database.Database.Db.Save(&responsavelEmpresa).Error
+	return
+}
+
+func DeleteResponsavelEmpresa(id int) (err error) {
+	err = database.Database.Db.Delete(&models.ResponsavelEmpresa{}, "id=", id).Error
+	return
+}
+
+func GetAllResponsavelEmpresa() (responsaveisEmpresa []models.ResponsavelEmpresa, err error) {
+	err = database.Database.Db.Find(&responsaveisEmpresa).Error
+	return
+}
